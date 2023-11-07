c90e33fcbde7e6796b355d18b82f71da5ad076b0
guilherm.henrique <guilherme.amaral.pro@gmail.com>
2023-11-07 08:35:59 +0100

    Verify if the location exist

---
+++ MVC_Client/Models/LocationM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Client.Models
+{
+    public class LocationM
+    {
+        [Required]
+        public string Street { get; set; }
+        public string No {  get; set; }
+        public int Postcode { get; set; }
+        public string City { get; set; }
+
+        public IFormFile formFile { get; set; }
+
+        public LocationM() { }
+
+        public LocationM(string street, string no, int postcode, string city, IFormFile formFile)
+        {
+            Street = street;
+            No = no;
+            Postcode = postcode;
+            City = city;
+            this.formFile = formFile;
+        }
+    }
+}
